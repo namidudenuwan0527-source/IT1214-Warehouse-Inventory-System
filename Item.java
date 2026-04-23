8528d2966c6d6174656155c3120037a87ee9313a
namidudenuwan0527-source <namidudenuwan0527@gmail.com>
2026-04-23 20:09:51 +0530

    Update Item.java

--- Item.java
+++ Item.java
@@ -92,46 +92,45 @@ class Warehouse{
 	
 	Scanner sc=new Scanner(System.in);
 	x=sc.nextInt();
-	
+		Scanner s3=new Scanner(System.in);
 	switch(x){
 	
 	case 0:
 		int c;
-		Scanner s1=new Scanner(System.in);
+		
 		System.out.print("Enter item name :");
-		String n=s1.nextLine();
+		String n=s3.nextLine();
 		System.out.print("Enter item Quantity :");
-		int q=s1.nextInt();
+		int q=s3.nextInt();
 		System.out.print("Enter item price :");
-		double p=s1.nextDouble();
+		double p=s3.nextDouble();
 		Inventory.addtoarraylist(index,n,q,p);
 		break;
+	
 	case 1:
 		int y;
-		Scanner s3=new Scanner(System.in);
+		
 		System.out.print("Enter index of item to remove :");
 		y=s3.nextInt();
 		Inventory.removefromarraylist(y);
 		break;
 	case 2:
-		Scanner s4=new Scanner(System.in);
-		Scanner s5=new Scanner(System.in);
-		Scanner s6=new Scanner(System.in);
+		
 		System.out.print("Enter index of item to update :");
-		int m=s4.nextInt();
+		int m=s3.nextInt();
 		
 		String de;
 		do{
 		System.out.println("for Add quantity enter 1\nfor remove quantity enter 2");
-		de=s5.nextLine();
+		de=s3.nextLine();
 		if(de.equals("1")){
 			System.out.print("Enter quantitiy to Add :");
-			Inventory.UpdateAddQuantitiy(m,s6.nextInt());
+			Inventory.UpdateAddQuantitiy(m,s3.nextInt());
 			break;
 		}
 		else if(de.equals("2")){
 			System.out.print("Enter quantitiy to remove :");
-			Inventory.UpdateremoveQuantitiy(m,s6.nextInt());
+			Inventory.UpdateremoveQuantitiy(m,s3.nextInt());
 			break;
 		}
 		else{
@@ -144,9 +143,9 @@ class Warehouse{
 		break;
 	case 3:
 			System.out.println("Enter ID of item for viwe details");
-			Scanner s7=new Scanner(System.in);
 			
-			Inventory.viwe(s7.nextInt());
+			
+			Inventory.viwe(s3.nextInt());
 		break;
 	case 4:
 			for(int r=0;r<Inventory.itemlist.size();++r){
